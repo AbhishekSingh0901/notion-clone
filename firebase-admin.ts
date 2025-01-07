e129f6aed778fc4795ac9787cb6fe4684be163f7
Abhishek Singh <as9284608@gmail.com>
2025-01-07 21:15:05 +0530

    resolving service key import bug

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -1,13 +1,19 @@
-import { initializeApp, getApps, getApp, App, cert } from "firebase-admin/app";
+import {
+  initializeApp,
+  getApps,
+  getApp,
+  App,
+  cert,
+  ServiceAccount,
+} from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const serviceKey = require("@/service_key.json");
+import * as serviceKey from "@/service_key.json";
 
 let app: App;
 
 if (getApps().length === 0) {
   app = initializeApp({
-    credential: cert(serviceKey),
+    credential: cert(serviceKey as ServiceAccount),
   });
 } else {
   app = getApp();
